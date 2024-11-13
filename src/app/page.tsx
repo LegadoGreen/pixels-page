faf61902ef4bc8b3c47c423b95f4efc26c812cb8
DavidsDvm <davids10.dvm@gmail.com>
2024-11-13 13:50:25 -0500

    Feat: new styles and toast container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Import Toast styles
 import CanvasContainer from "./components/CanvasConainer"; "@/components/CanvasConainer";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeButton
+      />
       <CanvasContainer />
     </main>
   );
